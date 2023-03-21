53c93d5c46f35d813f1e49fd4f51d1189505235e
dgj-snowflake <127446178+dgj-snowflake@users.noreply.github.com>
2023-03-20 22:11:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,6 @@ streamlit.dataframe(fruits_to_show)
 
 
 #New Section to display fruityvice api response
-import results
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
